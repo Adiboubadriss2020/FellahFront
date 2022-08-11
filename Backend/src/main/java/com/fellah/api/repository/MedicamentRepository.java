c0b7cefb70a6bedcd40c1168c0b43ac70f613cbc
Adib Oubadriss <61279217+Adiboubadriss2020@users.noreply.github.com>
2022-08-11 12:20:29 +0000

    Delete Backend directory

--- Backend/src/main/java/com/fellah/api/repository/MedicamentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fellah.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fellah.api.model.Medicament;
-
-public interface MedicamentRepository extends JpaRepository<Medicament, Integer> {
-
-}

d20798c52e2746ec979f35bdbfe15c2cc092273c
Adib Oubadriss <61279217+Adiboubadriss2020@users.noreply.github.com>
2022-08-11 11:27:32 +0000

    Delete Back directory

--- Back/Fellah-back/README.md
+++
@@ -1,3 +0,0 @@
-# Fellah_back
-
-New push-backend Fellah app
--- Back/Fellah-back/src/main/java/com/fellah/api/repository/ActiviteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fellah.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.fellah.api.model.Activite;
-
-public interface ActiviteRepository extends JpaRepository<Activite, Integer> {
-
-}
